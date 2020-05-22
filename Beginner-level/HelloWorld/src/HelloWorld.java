ebe2d12e963ae5e14a70d274bf367a74add1c0c5
MyWorld <arunpnambiar26@outlook.com>
2020-05-22 16:26:12 +0530

    HelloWorld program commit

--- Beginner-level/HelloWorld/src/HelloWorld.java
+++ Beginner-level/HelloWorld/src/HelloWorld.java
@@ -3,3 +3,17 @@ public class HelloWorld {
         System.out.println("Hello world !!!!!!!!!");
     }
 }
+/*
+   Java is object oriented programing language.
+   What is a object? the answer for this question is object has state and behaviour.
+   for real world example car is a object. it has different behaviours like  drive, horn etc.
+  What is a class? the answer for this question is class is a blueprint of objects. it contains variables and method.
+  In the above program Helloworld is a program to print Hello world in console. In java the entry point is main method.
+  Main method always public and static. because while running ]ava it create .class file. we cannot call class with out object.
+  But if we make a class as static we can call by class name. for that main method always static.
+  Public: it is a access specifier , public means we can access from outside world.
+  System  means a class
+  out means a object
+  println is a method.
+  so System.out.println() ->> means println is method inside PrintStream class and out is object of PrintStream created inside System class
+ */
